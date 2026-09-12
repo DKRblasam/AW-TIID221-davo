7efcc1c87a5a33997045c8e82c88f5a25472f423
Davolin <fer.mtz.guz@gmail.com>
2026-09-11 23:56:10 -0600

    feat: add my first file in python

---
+++ Practica-prueba-git/Helloworld.py
@@ -0,0 +1,5 @@
+print ("Hello world; This is my first program in Python!")
+print ("My name is: Juan Pablo")
+print ("I'm a student of the course: ITIID 221")
+print ("I like to learn programming languages and I want to be a software developer in the future.")
+print ("I'm Studying in the University Politecnic of QRO, and I'm in my 4th cuatri of my career.")
